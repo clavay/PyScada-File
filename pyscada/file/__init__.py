cc799a4acfafe7f8005ba1f45a4f733aa6056f85
clavay <clavayssiere@univ-pau.fr>
2023-04-05 11:41:24 +0200

    add project informations
    use project informations
    update license and pyscada version

--- pyscada/file/__init__.py
+++ pyscada/file/__init__.py
@@ -3,16 +3,16 @@ from __future__ import unicode_literals
 
 import pyscada
 
-__version__ = '0.7.1rc1'
-__author__ = 'Camille Lavayssière'
-
-plugin_name = "File"
-plugin_name_lower = plugin_name.lower()
+__version__ = "0.8.0"
+__author__ = "Camille Lavayssiere"
+__email__ = "team@pyscada.org"
+__description__ = "File extension for PyScada a Python and Django based Open Source SCADA System"
+__app_name__ = "File"
 
 PROTOCOL_ID = 14
 
 parent_process_list = [{'pk': PROTOCOL_ID,
-                        'label': 'pyscada.' + plugin_name_lower,
-                        'process_class': 'pyscada.' + plugin_name_lower + '.worker.Process',
+                        'label': 'pyscada.' + __app_name__.lower(),
+                        'process_class': 'pyscada.' + __app_name__.lower() + '.worker.Process',
                         'process_class_kwargs': '{"dt_set":30}',
                         'enabled': True}]
